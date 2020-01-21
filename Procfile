5b147938506a6a44bd0fd73f57975703bcf253bd
Misha <mglsh@ukr.net>
2020-01-22 01:48:36 +0200

    3

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java $JAVA_OPTS -jar target/serverV4 0.0.1-SNAPSHOT.jar --server.port=$PORT $JAR_OPTS
+
---
+++ src/main/resources/db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE 	oci40tvyzisgmdju.device (
+
+id INT NOT NULL,
+
+name VARCHAR(255) NULL,
+
+email VARCHAR(255) NULL,
+
+phone VARCHAR(45) NULL,
+
+PRIMARY KEY (id));
+
+ALTER TABLE 	oci40tvyzisgmdju.device
+
+CHANGE COLUMN id id INT(11) NOT NULL AUTO_INCREMENT ,
+
+ADD UNIQUE INDEX id_UNIQUE (id ASC);
